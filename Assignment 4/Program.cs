3c685964e0f565a96e09ccf5f1399d4aaeff2364
Sara0921 <salahsara360@gmail.com>
2026-03-09 02:26:10 +0200

    Part01/Question01

--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Part 01
+            #region Question01
+            //Static Binding >>Resolved at compile time 
+            //Happens with: Method Overloading , Operator Overloading , Method Hiding
+            //Dynamic Binding >> Resolved at runtime
+            //Happens with: Method Overriding , Abstract Methods , Interface Implementation.
+            #endregion
+            #endregion
+
         }
     }
 }
